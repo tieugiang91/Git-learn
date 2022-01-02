ca400f2be9e8e051ad53e8b888dc6620d2945708
QuyNB182747 <quy.nb182747@sis.hust.edu.vn>
2022-01-02 16:50:25 +0700

    hay duoc di

---
+++ src/QuyNB_HelloWorld.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+<<<<<<< HEAD
+	printf("Hello World");
+	return 0;
+}
+
+=======
+	printf("%s", "Giang");
+	return 0;
+}
+
+
+
+>>>>>>> c8a920d39d3ecce144e352a9816383384e883e33
