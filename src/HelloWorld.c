ee9ef100de2330ae7d7e1635fa8fe645b94fe2a1
QuyNB182747 <quy.nb182747@sis.hust.edu.vn>
2022-01-02 17:01:26 +0700

    sorry mng

---
+++ src/HelloWorld.c
@@ -0,0 +1,25 @@
+<<<<<<< HEAD
+#include <stdio.h>
+
+int main()
+{
+<<<<<<< HEAD
+	printf("Hello World");
+	return 0;
+}
+
+=======
+	printf("%s", "Giang");
+	return 0;
+}
+
+
+
+>>>>>>> c8a920d39d3ecce144e352a9816383384e883e33
+=======
+#include<stdio.h>
+int main(){
+	printf("Hello World");
+	return 0;
+}
+>>>>>>> a74a65d261156930145ad6a9c8e2548fd759c253
